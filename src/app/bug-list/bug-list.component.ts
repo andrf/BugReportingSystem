555f0552dc2f957414ef419eb7b4a5df9186ac2b
Konstantinos Valiakas <konstantinos.valiakas@unify.com>
2020-09-21 17:34:40 +0300

    Add bug table

--- src/app/bug-list/bug-list.component.ts
+++ src/app/bug-list/bug-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Bug } from '../bug';
+import { BugReportSystemService } from '../bug-report-system.service';
 
 @Component({
   selector: 'app-bug-list',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bug-list.component.scss']
 })
 export class BugListComponent implements OnInit {
+  bugs: Bug[] = [];
 
-  constructor() { }
+  constructor(private bugReportSystemService: BugReportSystemService) { }
 
   ngOnInit(): void {
+    this.bugs = this.getBugs();
+  }
+
+  getBugs(): Bug[] {
+    // TODO:  Get bugs from bugReportSystemService
+    return [{
+      title: 'another bug',
+      priority: 1,
+      reporter: 'QA',
+      dateCreated: '2020-09-21T13:35:08.190Z',
+      status: 'Open'
+    }, {
+      title: 'bugota',
+      priority: 1,
+      reporter: 'QA',
+      dateCreated: '2020-09-21T13:23:14.060Z',
+      status: 'Done'
+    }];
   }
 
 }
